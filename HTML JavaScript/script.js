75d6c3590a7ac601b399862635d4090ad93261e6
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2024-02-01 14:33:46 +0700

    up

---
+++ HTML JavaScript/script.js
@@ -0,0 +1,13 @@
+function test() {
+    let a = 5;
+    let b = 10;
+    if (a > b) {
+        console.log(a > b);
+    } else {
+        console.log("b > a");
+    }
+}
+// function timmerFunc() {
+//     var now = new Date();
+//     var hour = new
+// }
